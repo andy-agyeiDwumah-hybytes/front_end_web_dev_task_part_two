04d57e6228cff1a79366f0a4015db7821769a3b2
Andy Agyei-Dwumah <andydwumah@gmail.com>
2025-02-13 21:50:38 +0000

    Get date full year inside component instead of outside

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,9 @@
-const d = new Date();
-const currentYear = d.getFullYear();
-
 export default function Footer({styles}) {
   return (
     <footer className={styles.homeFooter}>
       <p className={styles.footerPara}>
         <strong>
-          <em>Copyright &copy; {currentYear}</em>
+          <em>Copyright &copy; {new Date().getFullYear()}</em>
         </strong>
       </p>
     </footer>
